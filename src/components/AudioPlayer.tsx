1e6587a5e5de579d8bbebfa28d6fe8ffff6ae7b0
Aryan Singh <aryansingh979211@gmail.com>
2026-04-23 03:29:11 +0530

    Changed background music from ambient to nature sounds

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -35,7 +35,7 @@ const AudioPlayer = forwardRef<AudioPlayerHandle>((props, ref) => {
 
   return (
     <div className="fixed bottom-6 right-6 z-50">
-      <audio ref={audioRef} src="/ambient.wav" loop />
+      <audio ref={audioRef} src="/nature.wav" loop />
       <button 
         onClick={toggleMute}
         title={isPlaying ? "Mute Background Audio" : "Play Background Audio"}
